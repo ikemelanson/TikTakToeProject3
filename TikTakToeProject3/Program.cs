50617be7084cb2828e6619e3d7c3472a45e2a215
Ike Melanson <isaacmel@byu.edu>
2023-01-23 13:04:06 -0700

    Add project files.

---
+++ TikTakToeProject3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TikTakToeProject3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
