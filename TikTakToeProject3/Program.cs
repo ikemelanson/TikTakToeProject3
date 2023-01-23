18959f2d9c4096971eef3de4dd112ac0ad990b84
Parker Warner <plouwarner@gmail.com>
2023-01-23 13:41:56 -0700

    Main program structure

--- TikTakToeProject3/Program.cs
+++ TikTakToeProject3/Program.cs
@@ -6,12 +6,25 @@ namespace TikTakToeProject3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
+
+
+            // Welcome the user to the game
+            Console.WriteLine("Hello, Welcome to Gorup 1's Tic Tac Toe Game!");
+
+            // Create a game board array to store the players’ choices
+
+            char[,] gameBoard = new char[3,3];
+
+            char x = gameBoard[0,0] = 'A';
+
+            do
+            {
+                Supporting.printBoard()
+            }
+            while (Supporting.checkWin() == false);
 
             /*
-            • Welcome the user to the game
-            • Create a game board array to store the players’ choices
             • Ask each player in turn for their choice and update the game board array
             • Print the board by calling the method in the supporting class
             • Check for a winner by calling the method in the supporting class, and notify the players
@@ -19,6 +32,6 @@ namespace TikTakToeProject3
             */
 
 
-    }
+        }
     }
 }
